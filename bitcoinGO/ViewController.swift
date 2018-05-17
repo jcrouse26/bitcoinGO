742fe08199d4716bb8be5e8ed9d799b94d030f92
jcrouse26 <jcrouse26@gmail.com>
2018-05-17 12:54:12 -0700

    Initial Commit

---
+++ bitcoinGO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bitcoinGO
+//
+//  Created by Jason Crouse on 5/17/18.
+//  Copyright © 2018 Jason Crouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
