8c57c0ffa312b7f539d6987e0936731af4aec5c4
jcrouse26 <jcrouse26@gmail.com>
2018-05-24 15:33:15 -0700

    phone authentication working

---
+++ bitcoinGO/LabelExtension.swift
@@ -0,0 +1,20 @@
+//
+//  LabelExtension.swift
+//  bitcoinGO
+//
+//  Created by Jason Crouse on 5/22/18.
+//  Copyright © 2018 Jason Crouse. All rights reserved.
+//
+
+import UIKit
+
+/*extension UILabel {
+    func addCharacterSpacing() {
+        if let labelText = text, labelText.count > 0 {
+            let attributedString = NSMutableAttributedString(string: labelText)
+            attributedString.addAttribute(NSAttributedStringKey.kern, value: 4.7, range: NSRange(location: 0, length: attributedString.length - 1))
+            attributedText = attributedString
+        }
+    }
+}
+*/
--- bitcoinGO/ViewController.swift
+++ bitcoinGO/ViewController.swift
@@ -7,19 +7,93 @@
 //
 
 import UIKit
+import FacebookLogin
+import FacebookCore
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LoginButtonDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func facebookButton(_ sender: Any) {
+        let loginManager = LoginManager()
+        
+        loginManager.logIn(readPermissions: [ .publicProfile, .email, .userLocation], viewController: self) { LoginResult in
+            switch LoginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("user login cancelled")
+            case .success( _, _, _):
+                print(" ")
+                let credential = FacebookAuthProvider.credential(withAccessToken: (AccessToken.current?.authenticationToken)!)
+                Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
+                    if error != nil {
+                        print(error?.localizedDescription as Any)
+                        return
+                    }
+                    // User is signed in
+                    print("success, user signed in")
+                }
+            }
+        }
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if AccessToken.current != nil {
+            // User is logged in, use 'accessToken' here.
+            print("user ", AccessToken.current?.userId as Any, "is logged in")
+            
+            //let MapViewController = ViewController(nibName: "MapViewController", bundle: nil)
+            //self.navigationController?.pushViewController(MapViewController, animated: true)
+            
+        }
+        
+    }
+    
+    
+    
+    func loginButtonDidCompleteLogin(_ loginButton: LoginButton, result: LoginResult) {
+        // some code here
+        print("loginButtonDidCompleteLogin")
+        let credential = FacebookAuthProvider.credential(withAccessToken: (AccessToken.current?.authenticationToken)!)
+        
+        Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
+            if error != nil {
+                print(error?.localizedDescription as Any)
+                return
+            }
+            // User is signed in
+        }
+        
+        showEmailAddress()
+        
+    }
+    func showEmailAddress() {
+        let accessToken = AccessToken.current
+        
+        guard let accessTokenString = accessToken?.authenticationToken else { return }
+        
+        let credentials = FacebookAuthProvider.credential(withAccessToken: accessTokenString)
+        
+        Auth.auth().signInAndRetrieveData(with: credentials) { (user, error) in
+            if error != nil {
+                print("ya fucked up", error as Any)
+            }
+            print("successfully logged in with", user as Any)
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func loginButtonDidLogOut(_ loginButton: LoginButton) {
+        // some code here
+        
     }
-
 
 }
 
