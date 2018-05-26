8b9f4608c9385fccd4f64bd2b87e8b9f78606b72
jcrouse26 <jcrouse26@gmail.com>
2018-05-25 18:43:13 -0700

    update signUpView Controller

--- bitcoinGO/SignUpViewController.swift
+++ bitcoinGO/SignUpViewController.swift
@@ -11,49 +11,15 @@ import Firebase
 
 
 class SignUpViewController: UIViewController {
-    @IBOutlet weak var email: UITextField!
-    
-    @IBOutlet weak var password: UITextField!
     
     @IBOutlet weak var phoneNumber: UITextField!
-    
     @IBOutlet weak var verificationCode: UITextField!
-    
     @IBOutlet weak var status: UILabel!
-    
-    
-    @IBOutlet weak var emailSignUp: UIButton!
-    @IBAction func emailSignUp(_ sender: Any) {
-        let actionCodeSettings = ActionCodeSettings()
-        actionCodeSettings.url = URL(string: "https://wt9w4.app.goo.gl/eNh4")
-        // The sign-in operation has to always be completed in the app.
-        actionCodeSettings.handleCodeInApp = true
-        actionCodeSettings.setIOSBundleID(Bundle.main.bundleIdentifier!)
-        actionCodeSettings.setAndroidPackageName("com.example.android",
-                                                 installIfNotAvailable: false, minimumVersion: "12")
-        
-        Auth.auth().sendSignInLink(toEmail:email.text!, actionCodeSettings: actionCodeSettings) { error in
-            // ...
-            if let error = error {
-                self.status.text = error.localizedDescription
-                print(error.localizedDescription)
-                return
-            }
-            // The link was successfully sent. Inform the user.
-            // Save the email locally so you don't need to ask the user for it again
-            // if they open the link on the same device.
-            UserDefaults.standard.set(self.email.text, forKey: "Email")
-            self.status.text = "Check your email for link"
-            print("check your email for link")
-            // ...
-        }
-        
-    }
-    
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var verifyButton: UIButton!
     
     var verification : Verification!
     var applicationKey = "3924773d-fb21-4f1b-af25-f460e911c343"
-    
 
     @IBAction func smsVerification(_ sender: Any) {
         self.disableUI(true);
@@ -66,15 +32,34 @@ class SignUpViewController: UIViewController {
             }
             // save verification ID
             UserDefaults.standard.set(verificationID, forKey: "authVerificationID")
-            
+            self.phoneNumber.isHidden = true
+            self.nextButton.isHidden = true
+            self.verifyButton.isHidden = false
+            self.verificationCode.isHidden = false
         }
         
     }
     
-    @IBAction func verify(_ sender: Any) {
+    @IBAction func didTapVerify(_ sender: Any) {
+        verify()
+    }
+    
+    
+    func verify() {
+        
+        // If user has been verified before, enter game
+        if UserDefaults.standard.bool(forKey: "phoneVerified") == true {
+            print("user already verified via phone")
+            self.performSegue(withIdentifier: "segueToMapView", sender: self)
+        }
         
         // Restore verification ID
         let verificationID = UserDefaults.standard.string(forKey: "authVerificationID")
+        
+        // If nil return
+        if verificationID == nil {
+            return
+        }
         
         // Sign in with verification ID and user-inputted code
         let credential = PhoneAuthProvider.provider().credential(
@@ -88,7 +73,8 @@ class SignUpViewController: UIViewController {
                 return
             }
             // User is signed in
-            self.status.text = "Verified"
+            UserDefaults.standard.set(true, forKey: "phoneVerified")
+            self.performSegue(withIdentifier: "segueToMapView", sender: self)
         }
     }
     
@@ -116,13 +102,22 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         // Do any additional setup after loading the view.
+        verificationCode.isHidden = true
+        verifyButton.isHidden = true
+        
+        UserDefaults.standard.set(false, forKey: "phoneVerified")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        verify()
     }
     
 
@@ -135,5 +130,33 @@ class SignUpViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    // Should we want to do email verification:
+    
+    /*@IBAction func emailSignUp(_ sender: Any) {
+     let actionCodeSettings = ActionCodeSettings()
+     actionCodeSettings.url = URL(string: "https://wt9w4.app.goo.gl/eNh4")
+     // The sign-in operation has to always be completed in the app.
+     actionCodeSettings.handleCodeInApp = true
+     actionCodeSettings.setIOSBundleID(Bundle.main.bundleIdentifier!)
+     actionCodeSettings.setAndroidPackageName("com.example.android",
+     installIfNotAvailable: false, minimumVersion: "12")
+     
+     Auth.auth().sendSignInLink(toEmail:email.text!, actionCodeSettings: actionCodeSettings) { error in
+     // ...
+     if let error = error {
+     self.status.text = error.localizedDescription
+     print(error.localizedDescription)
+     return
+     }
+     // The link was successfully sent. Inform the user.
+     // Save the email locally so you don't need to ask the user for it again
+     // if they open the link on the same device.
+     UserDefaults.standard.set(self.email.text, forKey: "Email")
+     self.status.text = "Check your email for link"
+     print("check your email for link")
+     // ...
+     }
+     
+     }*/
 }
